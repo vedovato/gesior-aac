0e3a9017237de4c6fd6e5ad409f3ff1d0b6c6e32
Majesty <32709570+MajestyOTBR@users.noreply.github.com>
2021-11-25 21:52:27 -0300

    Update insertAccount.sql

--- pages/accountmanagement/sellchar-util/insertAccount.sql
+++ pages/accountmanagement/sellchar-util/insertAccount.sql
@@ -3,7 +3,5 @@ INSERT INTO `accounts`
  `email_new_time`, `rlname`, `location`, `page_access`, `email_code`, `next_email`, `create_date`, `create_ip`, `last_post`, `flag`,
  `guild_points`, `guild_points_stats`, `passed`, `block`, `refresh`, `birth_date`, `gender`, `loyalty_points`, `authToken`)
 VALUES
-	(2, 'sellcharacters', sha1('#@!!@##@!@!#ABCD'), null, 0, 1, 0, 0, 0, 'sell@email.com', 0, 0, 0, 0, 0, 0, '', 0, 0, 0,
-																																														0, 0, 0, '', 0, 0, 0, 0, 0,
-																																															 '', 'm', 0, '');
+(2, 'sellcharacters', sha1('#@!!@##@!@!#ABCD'), null, 0, 1, 0, 0, 0, 'sell@email.com', 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, 0, '', 0, 0, 0, 0, 0, '', 'm', 0, '');
 
